31da3565c8f1b3f0cc074b4775eee48941de9ef8
Movsisyan <mhermovsisyan1988@gmail.com>
2018-10-07 15:38:08 +0400

    Remove unnecessary files

---
+++ Test/Test/Supporting Files/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Test
+//
+//  Created by Mher Movsisyan on 10/6/18.
+//  Copyright © 2018 Mher Movsisyan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
